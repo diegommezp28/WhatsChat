ef7c880ba0aca03b84570f0dbd069d55549a944f
diegommezp28 <da.gomezp@uniandes.edu.co>
2022-08-30 15:21:23 -0500

    Can send messages from a csv list

--- assets/js/inject.js
+++ assets/js/inject.js
@@ -1,30 +1,50 @@
 // This is the code that will actually send the given template to 
 // the specified contacts on the csv input file
 
+async function sleep(ms = 1000) { return new Promise(resolve => setTimeout(resolve, ms)) };
+
 /**
  * 
  * @param {*} message 
  * @param {*} number 
  */
-function sendMessageTo(number, message) {
+async function sendMessageTo(number, message) {
     let numberLink = document.createElement("a");
     numberLink.id = "number_link";
     document.body.append(numberLink);
-    numberLink.setAttribute("href", `https://api.whatsapp.com/send?phone=${number}&text=`)
+    numberLink.setAttribute("href", `https://api.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(message)}`)
+    numberLink.click()
+    await sleep(3000);
+    let sendButton = document.querySelector('[data-testid="compose-btn-send"]');
+    console.log(sendButton);
+    sendButton.click();
+    await sleep(500);
 
-    setTimeout(() => numberLink.click(), () => {
-        numberLink.remove();
-    }, 0)
+}
+
+async function sendAllMessages(messageTemplate, csvList) {
+    // let firstNumber = csvList[0][1];
+    // sendMessageTo(firstNumber, formatMessage(messageTemplate, csvList[0]));
+
+    for (let i = 0; i < csvList.length; i++) {
+        let number = csvList[i][1];
+        let name = csvList[i][0];
+
+        await sendMessageTo(number, formatMessage(messageTemplate, csvList[i]));
+
+
+    }
 
 }
 
 
 function formatMessage(messageTemplate, fields) {
+    return messageTemplate;
 
 }
 
-//input field for template
-//input field to upload csv
+//input field for template -->
+//input field to upload csv -->
 // Format message (validate csv, validate numbers, replace fields),
 // find text box, edit text box, send message, wait a little bit, repeat
 //Message preview
@@ -34,3 +54,14 @@ function formatMessage(messageTemplate, fields) {
 //Stats and % of succesfully sent messages
 
 //Release
+
+// let messageTemplate = chrome.storage.sync.get({ messageTemplate });
+chrome.storage.sync.get(['csvList'], (result) => {
+    let csvList = result.csvList;
+    console.log(csvList)
+    chrome.storage.sync.get(['messageTemplate'], (result) => {
+        let messageTemplate = result.messageTemplate;
+        console.log(messageTemplate);
+        sendAllMessages(messageTemplate, csvList)
+    });
+});
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,50 +1,109 @@
-function sendMessageTo(message, number) {
-    let numberLink = document.createElement("a");
-    numberLink.id = "number_link";
-    document.body.append(numberLink);
-    numberLink.setAttribute("href", `https://api.whatsapp.com/send?phone=${number}&text=`)
+let csv_text = document.getElementById("csv_text");
+let csv_file = document.getElementById("csv_file");
+let send_button = document.getElementById("send_button");
+let message_template = document.getElementById("message");
 
-    setTimeout(() => numberLink.click(), () => {
-        numberLink.remove();
-    }, 0)
+csv_file.addEventListener("change", handleFileSelect);
+send_button.addEventListener("click", handleMessageSubmit);
 
-}
-
-function sendCurrentMessage() {
-    console.log("Messaging");
-    chrome.storage.sync.get(['messageData'], (result) => {
-        [message, phoneNumber] = result.messageData
-        console.log('Value currently is ' + result.messageData);
-        // sendMessageTo(message, phoneNumber)
-
-        let numberLink = document.createElement("a");
-        numberLink.id = "number_link";
-        document.body.append(numberLink);
-        numberLink.setAttribute("href", `https://api.whatsapp.com/send?phone=${phoneNumber}&text=`)
-
-        setTimeout(() => numberLink.click(), () => {
-            numberLink.remove();
-        }, 0)
-    });
-
-    // sendMessageTo(message, phoneNumber)
-
-}
-
-let sendButton = document.getElementById("send_button");
-sendButton.addEventListener("click", async () => {
+async function handleMessageSubmit() {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    let message = document.getElementById("message").value;
-    let phoneNumber = document.getElementById("cell_input").value;
-    console.log("Data:", message, phoneNumber);
-    chrome.storage.sync.set({ messageData: [message, phoneNumber] });
+
+    let messageTemplate = message_template.value;
+    let contacts_csv = csv_text.value;
+
+    csvList = csvToArray(contacts_csv);
+
+    chrome.storage.sync.set({ messageTemplate });
+    chrome.storage.sync.set({ csvList });
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: sendCurrentMessage,
+        files: ["assets/js/inject.js"]
     });
+}
 
-})
+function handleFileSelect(evt) {
+    let file = evt.target.files[0];
+    let reader = new FileReader();
+
+    reader.onload = function (event) {
+        let text = event.target.result;
+        csv_text.value = text;
+        console.log(text);
+        let csvList = csvToArray(text);
+        // TODO: add warning in case parsing fails
+        chrome.storage.sync.set({ csvList });
+    };
+    reader.readAsText(file);
+
+}
+
+function csvToArray(str, delimiter = ',', header = false) {
+
+    // slice from \n index + 1 to the end of the text
+    // use split to create an array of each csv value row
+    const rows = str.split("\n");
+
+    // split values from each row into an array
+    const arr = [];
+    for (let i = 0; i < rows.length; i++) {
+        arr.push(rows[i].split(','));
+    }
+    // TODO: add warning if parsing fails
+    return arr;
+
+}
+
+// send_button.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//     let messageTemplate = document.getElementById("message").value;
+//     chrome.storage.sync.set({ messageTemplate });
+
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         files: ["assets/js/inject.js"]
+//         // func: sendCurrentMessage,
+//     });
+
+// })
+
+
+
+// function sendMessageTo(message, number) {
+//     let numberLink = document.createElement("a");
+//     numberLink.id = "number_link";
+//     document.body.append(numberLink);
+//     numberLink.setAttribute("href", `https://api.whatsapp.com/send?phone=${number}&text=`)
+
+//     setTimeout(() => numberLink.click(), () => {
+//         numberLink.remove();
+//     }, 0)
+
+// }
+
+// function sendCurrentMessage() {
+//     console.log("Messaging");
+//     chrome.storage.sync.get(['messageData'], (result) => {
+//         [message, phoneNumber] = result.messageData
+//         console.log('Value currently is ' + result.messageData);
+//         // sendMessageTo(message, phoneNumber)
+
+//         let numberLink = document.createElement("a");
+//         numberLink.id = "number_link";
+//         document.body.append(numberLink);
+//         numberLink.setAttribute("href", `https://api.whatsapp.com/send?phone=${phoneNumber}&text=`)
+
+//         setTimeout(() => numberLink.click(), () => {
+//             numberLink.remove();
+//         }, 0)
+//     });
+
+//     // sendMessageTo(message, phoneNumber)
+
+// }
+
+
 
 
 
